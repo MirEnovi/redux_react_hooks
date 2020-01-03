518939b63c80c4eefa0493a4512a720c7aeb5847
mirian garcia <mirian@digitalcoaster.mx>
2020-01-03 14:37:57 -0600

    seccion 2 punto 5

---
+++ src/redux/userDuck.js
@@ -0,0 +1,16 @@
+// constanst
+let initialData = {
+    loggedIn: false
+}
+let LOGIN = "LOGIN";
+//reducer
+export default function reducer(state = initialData, action){
+    switch(action.type) {
+        /* case LOGIN:
+            return */
+        default:
+            return state
+    }
+}
+
+// action (action creator)
